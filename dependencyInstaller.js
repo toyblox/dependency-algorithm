8df22d8bd7e9ad0a38a2fa424e441d4a090d42e3
alicia chen <achen@overstock.com>
2017-10-03 18:58:13 -0600

    got the loops working - always forget to keep committing :S

--- dependencyInstaller.js
+++ dependencyInstaller.js
@@ -7,8 +7,7 @@ var packages = [
 	"Ice: "
 ];
 
-// packages to object array
-
+// Take input and parse into object of arrays
 var parseArray = function() {
 	var newObject = {};
 	packages.forEach(function(value) {
@@ -25,17 +24,60 @@ var parseArray = function() {
 	return newObject;
 };
 
-var newStuff = parseArray(packages);
+var arrayObject = parseArray(packages);
 
-console.log('newStuff', newStuff);
+console.log('array object', arrayObject);
 
 var recursiveInstaller = function(parsedArray) {
-    var installOrder = [];
-    var sorted = {};
+    var installOrder = [];	// This is returned to the user
+    var sorted = {};	// Keep track of already sorted packages
+    var key = Object.keys(parsedArray); // Get the keys inside parsed packages
 
-    Object.keys(parsedArray).forEach(function(key) {	// get the keys of the parsed packages, run them through loop
-      sort(key, []);
-      console.log('sorted[key]', sorted[key]);
-    });
 
-}
+    function sort(key, edges) {
+ 		// If package is already sorted, end this function
+ 		if (sorted[key])
+        return;
+
+    	// Package is new and has not been sorted
+    	// Add this package to edges array (this is frozen within the function)
+     	edges.push(key);
+    	// Get the current package based on name
+      	var currentPackage = parsedArray[key];
+
+      	// Loop through the dependencies in the package
+      	for (var i = 0; i < currentPackage.length; i += 1) {
+		  	// If the dependency already exists in the edges array
+		  	// (which is frozen based on which package is running)
+		  	// this means there is a circular dependency
+		  	if (edges.indexOf(currentPackage[i]) >= 0) {
+		  		throw 'CIRCULAR DEPENDENCY';
+		  	}
+			// This loops through all the dependencies in the current package
+			// There is no history of this dependency in the edges array
+			// Now take this dependency and run it through the sort function on its own
+			sort(currentPackage[i], edges);
+      	}
+
+		// If there is no history of this dependency in either the edges array or the sorted object,
+		// and it does not have dependencies
+		// then mark this package as sorted
+		sorted[key] = true;
+
+		// Add the sorted (terminal) package to the installOrder array
+		installOrder.push(key);
+    }
+
+    // Loop through each package
+    for (var i = 0; i < key.length; i += 1) {
+    	var packageName = key[i];
+    	// Run the sort function on the package
+    	sort(packageName, []);
+    }
+
+    return installOrder;
+ };
+
+  var packageInstaller = recursiveInstaller(arrayObject);
+
+  console.log('package install order', packageInstaller);
